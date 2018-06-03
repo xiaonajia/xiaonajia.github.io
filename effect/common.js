bff18f392b414e9714850651d39a87d3f979f387
xiaonajia <1787370981@q.com>
2018-06-03 11:47:34 +0800

    old

--- effect/common.js
+++ effect/common.js
@@ -168,3 +168,206 @@ function slidePlayClock(id){
 			}
 
 ===*/
+
+/*== 使用正则检测查找字符串中出现次数最多的字母和次数 ==*/
+function searchMaxStr(){
+	var str = 'dsfagfasssssfffagff';
+	var arr = str.split("");
+	str = arr.sort().join("");
+	
+	var index = 0;
+	var value = '';
+
+	var re = /(\w)\1+/g;
+	str.replace(re,function($0,$1){
+		if(index < $0.length){
+			index = $0.length;
+			value = $1;
+		}
+	});
+	console.log("出现次数最多的是：" + value + "次数是：" + index);
+}
+
+
+/*== 面向对象写选项卡切换 ==*/
+function Tab(id){
+	this.oParent = document.getElementById(id);
+	this.aInput = this.oParent.getElementsByTagName("input");
+	this.aDiv = this.oParent.getElementsByTagName("div");
+	this.iNow = 0;
+}
+Tab.prototype.init = function (){
+	var _this = this;
+	for(var i = 0; i < this.aInput.length; i++){
+		this.aInput[i].index = i;
+		this.aInput[i].onclick = function (){
+			 _this.change(this);
+		};
+	}
+}
+Tab.prototype.change = function (obj){
+	for(var j = 0; j < this.aInput.length; j++){
+		this.aDiv[j].style.display = 'none';
+		this.aInput[j].className = '';
+	}
+	this.aDiv[obj.index].style.display = 'block';
+	obj.className = 'active';
+}
+
+Tab.prototype.autoPlay = function (){
+	var _this = this;
+	setInterval(function (){
+		if(_this.iNow == _this.aInput.length - 1){
+			_this.iNow = 0;
+		}else{
+			_this.iNow ++ ;
+		}
+		for(var j = 0; j < _this.aInput.length; j++){
+			_this.aDiv[j].style.display = 'none';
+			_this.aInput[j].className = '';
+		}
+		_this.aDiv[_this.iNow].style.display = 'block';
+		_this.aInput[_this.iNow].className = 'active';
+		
+	},2000);
+}
+
+/*== 面向对象写  拖拽 切换 ==*/
+function Drag(){
+	this.oDrag = null;
+	this.disX = 0;
+	this.disY = 0;
+}
+
+Drag.prototype.init = function (id,toDown,toUp){
+	var _this = this;
+	this.oDrag = document.getElementById(id);
+	this.oDrag.onmousedown = function(ev){
+		var ev = ev || window.event; //ev是事件才有的，所以应该写在这儿，firefox
+		if(toDown) toDown();
+		_this.fnDown(ev);
+		document.onmousemove = function (ev){
+			var ev = ev || window.event;
+			_this.fnMove(ev);
+		};
+		document.onmouseup = function (){
+			if(toUp) toUp();
+			_this.fnUp();
+		};
+		return false; //阻止默认事件，也应该写在事件内部，chrome
+	};
+}
+
+Drag.prototype.fnDown = function (ev){
+	var _this = this;
+	this.disX = ev.clientX - this.oDrag.offsetLeft;
+	this.disY = ev.clientY - this.oDrag.offsetTop;
+
+	if(this.oDrag.setCapture){
+		this.oDrag.setCapture();
+	}
+}
+
+Drag.prototype.fnMove = function (ev){
+	this.oDrag.style.left = ev.clientX - this.disX + 'px';
+	this.oDrag.style.top = ev.clientY - this.disY + 'px';
+}
+
+Drag.prototype.fnUp = function (){
+	document.onmousemove = null;
+	document.onmouseup =  null;
+	if(this.relaseCapture){
+		this.relaseCapture();
+	}
+}
+
+/*== 拷贝继承-- ==*/
+function ChildDrag(id){
+	Drag.call(this,id);
+}
+extend(ChildDrag.prototype, Drag.prototype);
+
+ChildDrag.prototype.fnMove = function (ev){ //改写子类方法，限制范围
+	var L = ev.clientX - this.disX;
+	var T = ev.clientY - this.disY;
+	if(L < 0){
+		L = 0;
+	}else if(L > document.documentElement.clientWidth - this.oDrag.offsetWidth){
+		L = document.documentElement.clientWidth - this.oDrag.offsetWidth;
+	}
+
+	if(T < 0){
+		T = 0;
+	}else if(T > document.documentElement.clientHeight - this.oDrag.offsetHeight){
+		T = document.documentElement.clientHeight - this.oDrag.offsetHeight;
+	}
+
+	this.oDrag.style.left = L + 'px';
+	this.oDrag.style.top = T + 'px';
+}
+
+/*== 把一个对象拷贝赋值给另一个对象 ==*/
+function extend(obj1,obj2){
+	for(var attr in obj2){
+		obj1[attr] =  obj2[attr];
+	}
+}
+
+/*== 弹窗组件 ==*/
+function Dialog(){
+	this.oDiaglog = null;
+	this.settings = {
+		w: 300,
+		h: 300,
+		pos: 'center',
+		title: ''
+	}
+}
+Dialog.prototype.init = function (obj){
+	extend(this.settings, obj);
+	this.create();
+	this.fnClose();
+}
+Dialog.prototype.create = function (){
+	this.oDiaglog = document.createElement('div');
+	this.oDiaglog.className = 'dialog';
+	this.oDiaglog.innerHTML = '<div class="title"><span>'+ this.settings.title +'</span><span class="close">X</span></div><div class="con"></div>';
+	document.body.appendChild(this.oDiaglog);
+
+	this.setData();
+}
+Dialog.prototype.setData = function (){
+	this.oDiaglog.style.width = this.settings.w + 'px';
+	this.oDiaglog.style.height = this.settings.h + 'px';
+
+	if(this.settings.pos == 'center'){
+		this.oDiaglog.style.left = (viewWith()- this.oDiaglog.offsetWidth ) /2 + 'px';
+		this.oDiaglog.style.top = (viewHeight() - this.oDiaglog.offsetHeight ) /2 + 'px';
+	}
+	else if(this.settings.pos == 'right'){
+		this.oDiaglog.style.left = (viewWith()- this.oDiaglog.offsetWidth )  + 'px';
+		this.oDiaglog.style.top = (viewHeight() - this.oDiaglog.offsetHeight )  + 'px';
+	}
+}
+Dialog.prototype.fnClose = function (){
+	var oClose = this.oDiaglog.getElementsByTagName("span")[1];
+	var _this = this;
+	oClose.onclick = function (){
+		document.body.removeChild(_this.oDiaglog);
+	}
+}
+
+
+function viewWith(){
+	return document.documentElement.clientWidth;
+}
+function viewHeight(){
+	return document.documentElement.clientHeight; 
+}
+
+
+
+
+
+
+
